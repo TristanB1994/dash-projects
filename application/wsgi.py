f02c5a3b117dd7f6f120f5d378a26b0ae333a63c
Tristan <t.blandford.dev@gmail.com>
2021-04-28 14:17:02 -0400

    set dockerfile to serve dash via uwsgi

--- application/wsgi.py
+++
@@ -1,3 +0,0 @@
-import os
-
-from application.app import app
---
+++ run.py
@@ -0,0 +1,6 @@
+from application import app
+
+server = app.server
+#app.run_server(debug=True, host="0.0.0.0")
+#if __name__ == "__main__":
+#    app.run_server(debug=True, host="0.0.0.0")
